a05fbc7091be3e3e7ca2458d10c575caf8f4577e
K Agajanian <41064815+Erutis@users.noreply.github.com>
2022-06-04 10:30:21 -0400

    streamlit app draft

---
+++ streamlit/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("TLAS")
+st.header("Thought Leadership as a Service")
+st.subheader("Don\'t know what to post today? Why not have an AI help?")
+
+
+st.button('Generate Thought Leadership')
+
+
