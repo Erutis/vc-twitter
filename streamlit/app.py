3b9134c2141dc75a9cd93ad93f1c97bfff08cfb7
K Agajanian <41064815+Erutis@users.noreply.github.com>
2022-06-06 17:04:51 -0400

    update notes and streamlit

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,10 +1,14 @@
 import streamlit as st
+#import gpt_2_simple as gpt2
+from text_gen import generate_text
 
 st.title("TLAS")
 st.header("Thought Leadership as a Service")
 st.subheader("Don\'t know what to post today? Why not have an AI help?")
 
 
-st.button('Generate Thought Leadership')
+st.button('Generate Thought Leadership', on_click=generate_text)
+
+# function to run the button using the fine-tuned model run2
 
 
